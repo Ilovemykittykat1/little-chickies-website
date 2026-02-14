3e1cca2fed9cd6a56e30509e9f76e592c414d9f9
Katherine Mcneil <W0417834@nscc.ca>
2026-02-14 16:38:24 -0400

    redone

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-const images = document.querySelectorAll(".hero-image");
-let currentIndex = 0;
+// Only run slideshow on homepage
+if (document.querySelector(".hero")) {
+  const images = document.querySelectorAll(".hero-image");
+  let currentIndex = 0;
 
-setInterval(() => {
-  images[currentIndex].classList.remove("active");
-  currentIndex = (currentIndex + 1) % images.length;
-  images[currentIndex].classList.add("active");
-}, 4000);
+  setInterval(() => {
+    images[currentIndex].classList.remove("active");
+    currentIndex = (currentIndex + 1) % images.length;
+    images[currentIndex].classList.add("active");
+  }, 4000);
+}
+
+
 
 // HAMBURGER TOGGLE
 function toggleMenu() {
